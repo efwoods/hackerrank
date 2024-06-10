1ae10b933e4d363093142d1bc4ef0da4a1667c11
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-06-10 13:52:12 -0400

    updated efficient counting_valleys solution

--- InterviewPreparationKit/warmUpChallenges/counting_valleys.py
+++ InterviewPreparationKit/warmUpChallenges/counting_valleys.py
@@ -8,6 +8,9 @@ import os
 import random
 import re
 import sys
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='counting_valleys_debug.log')
 
 #
 # Complete the 'countingValleys' function below.
@@ -17,6 +20,51 @@ import sys
 #  1. INTEGER steps
 #  2. STRING path
 #
+
+def enter_valley(altitude: int, total_valleys: int):
+    """Indicates when a valley is entered. Updates the altitude and the
+    total number of valleys the user has encountered.
+
+    Args:
+        altitude (int): This is the current altitude.
+        total_valleys (int): This is a total of the number of valleys
+                             along a given path. 
+
+    Returns:
+        altitude (int): This is the current altitude.
+        total_valleys (int): This is a total of the number of valleys
+                             along a given path. 
+    """
+
+    altitude -= 1
+    total_valleys += 1
+
+    return altitude, total_valleys
+
+def change_altitude(altitude: int, step: str) -> ValueError | int:
+    """This method defines a change in the current altitude depending
+    upon a step.
+
+    Args:
+        altitude (int): This is the current altitude above sea level.
+        step (str): This is the current step to take. 'U' indicates an
+                    increase in altitude. 'D' indicates a decrease in
+                    altitude.
+    Returns:
+        altitude (int): This is the updated current altitude after
+                        taking a step.
+    """
+
+    if step == 'U':
+        altitude += 1
+    elif step == 'D':
+        altitude -= 1
+    else:
+        logging.error("step does not have a value of 'U' or 'D'")
+        logging.info(f'step: {step}')
+        return ValueError
+    
+    return altitude
 
 def countingValleys(steps: int, path: str) -> int:
     """A method that counts the number of valleys for a given path.
@@ -29,18 +77,17 @@ def countingValleys(steps: int, path: str) -> int:
     Returns:
         total_valleys (int): The number of valleys traversed.
     """
-    
-    # Map a D to -1 & U to +1
-    # Keep a running sum and increment a valley variable each time the
-    # sum is below zero.
-    
+
     total_valleys = 0
     altitude = 0
-    # for stepIndex in range(0, steps):
-    #     if
-    
+
+    for stepIndex in range(0, steps):
+        if altitude == 0 and path[stepIndex] == 'D':
+            altitude, total_valleys = enter_valley(altitude, total_valleys)
+        else:
+            altitude = change_altitude(altitude=altitude, step=path[stepIndex])
+
     return total_valleys
-    pass
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
