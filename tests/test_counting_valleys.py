4f3881f3a2c1c76edc657e018b676148eb4cf6e4
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-06-10 12:15:10 -0400

    added test for counting valleys

---
+++ tests/test_counting_valleys.py
@@ -0,0 +1,19 @@
+"""Module used to test counting valleys module."""
+
+# Imports
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                ".."))
+
+class TestCountingValleys(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+if __name__ == '__main__':
+    unittest.main()
