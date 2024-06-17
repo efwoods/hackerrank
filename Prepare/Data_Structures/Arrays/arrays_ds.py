4bbf53f2dd103b936101d5a9b212302768076b8b
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-06-17 12:35:41 -0400

    added solution for array_ds that reverses arrays and test cases for that solution

---
+++ Prepare/Data_Structures/Arrays/arrays_ds.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+"""This module reverses an array."""
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    """This method reverses an array.
+
+    Args:
+        a (list int): This is a list of integers to reverse.
+    """
+    a.reverse()
+
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+    
---
+++ tests/test_arrays_ds.py
@@ -0,0 +1,37 @@
+"""Module to test reverseArray method from the arrays_ds module."""
+
+import os
+import sys
+import logging
+import unittest
+
+logging.basicConfig(level=logging.DEBUG, filename='test_arrays_ds_output.log')
+sys.path.insert(0, 
+                os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
+from Prepare.Data_Structures.Arrays import arrays_ds
+
+class TestArraysDS(unittest.TestCase):
+    """This class contains tests that test the arrays_ds module.
+
+    Args:
+        unittest (module): This module enables testing within the class.
+    """
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test01_test_reverse_array(self):
+        """This tests the reverseArrays method with a base case.
+        """
+
+        arr = [1, 2, 3, 4]
+        result = arrays_ds.reverseArray(arr)
+        self.assertEqual(result, [4, 3, 2, 1])
+        
+
+if __name__ == '__main__':
+    unittest.main()
