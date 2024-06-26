fdf16d68a06399e9839f385913955838b2b9921c
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-06-26 16:26:02 -0400

    initialized tree_huffman_decoding module

---
+++ Prepare/Data_Structures/Trees/tree_huffman_decoding.py
@@ -0,0 +1,9 @@
+"""This module creates a huffman tree data structure."""
+
+def main() -> None:
+    """This is the main function of the module.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
